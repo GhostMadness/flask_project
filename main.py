613d063634d67973d2d8efa367d61943bbdbcd0b
GhostMadness <ghostmadness55@gmail.com>
2023-04-24 21:10:31 +0300

    добавил страшных вещей

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from data import db_session
+from data.users import User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
 
 @app.route('/')
 def index():
@@ -8,4 +12,5 @@ def index():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
     app.run(port=8000, host='127.0.0.1', debug=True)
